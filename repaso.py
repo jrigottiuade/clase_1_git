2bfa6395210a8e8d4353741589b990eaf0eac488
jrigotti <juan.rigotti@bue.edu.ar>
2026-02-11 23:10:55 -0300

    Mi primer commit

---
+++ repaso.py
@@ -0,0 +1,21 @@
+# Comentario en linea
+"""
+Comentario en bloque
+"""
+
+# Usamos print para impresion en terminal
+print("Calculadora en Python")
+
+# Inicializar variables
+# numero_1 = 4
+# numero_2 = 6
+
+numero_1 = int(input("Ingrese el primer numero: "))
+numero_2 = int(input("Ingrese el segundo numero: "))
+
+
+# Sumar numeros
+suma = numero_1 + numero_2
+
+# Mostrar suma
+print("La suma de los numeros es: " + str(suma))
