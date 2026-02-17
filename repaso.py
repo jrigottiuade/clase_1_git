d2b3e1da5696f63287b9217cee63ba632206d6d3
jrigottiuade <jrigotti@uade.edu.ar>
2026-02-17 08:34:09 -0300

    updated git config con datos de UADE

--- repaso.py
+++ repaso.py
@@ -4,7 +4,7 @@ Comentario en bloque
 """
 
 # Usamos print para impresion en terminal
-print("Calculadora en Python")
+print("Calculadora en Python - con config de UADE")
 
 # Inicializar variables
 # numero_1 = 4
@@ -18,4 +18,4 @@ numero_2 = int(input("Ingrese el segundo numero: "))
 suma = numero_1 + numero_2
 
 # Mostrar suma
-print("La suma de los numeros es: " + str(suma))
+print("La suma de los numeros: " + str(suma))
